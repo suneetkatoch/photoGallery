bab480b687f7fa6666495f37eb711fba52ff3bb7
Suneet <suneetkatoch7@gmail.com>
2025-09-01 13:17:41 +0530

    path change

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import auth from '../middleware/auth.js'
-import {uploadImageController} from '../controller/uploadImage.controller.js'
+import {uploadImageController} from '../controller/uploadimage.controller.js'
 import upload from '../middleware/multer.js'
 
 const uploadRouter = Router()
